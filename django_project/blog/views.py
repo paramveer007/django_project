0f72730997d532c30bfc7a67cdfcd112de1b2833
Paramveer Singh <ranga.param007@gmail.com>
2020-08-14 21:12:34 +0530

    add templates and add routes to those templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author' : 'Paramveer',
+        'title' : 'Blog Post 1',
+        'content' : 'First Post Content',
+        'date_posted' : 'August 14, 2020',
+    },
+    {
+        'author' : 'Mukul',
+        'title' : 'Blog Post 2',
+        'content' : 'Second Post Content',
+        'date_posted' : 'August 14, 2020',
+    }
+
+]
+
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Blog Page </h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})      # No need to create context. we can pass dictionary directly
